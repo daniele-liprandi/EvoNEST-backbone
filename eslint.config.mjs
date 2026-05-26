77615bdd997fe8208380d21ada79adc024e2d2d4
Daniele Liprandi <daniele.liprandi@gmail.com>
2026-05-26 18:53:05 +0200

    chore(lint): migrate to eslint v9 flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+
+export default [
+  ...nextCoreWebVitals,
+  {
+    rules: {
+      "react-hooks/error-boundaries": "off",
+      "react-hooks/immutability": "off",
+      "react-hooks/purity": "off",
+      "react-hooks/set-state-in-effect": "off",
+      "react-hooks/static-components": "off",
+      "react-hooks/use-memo": "off",
+    },
+  },
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "coverage/**",
+    ],
+  },
+];
